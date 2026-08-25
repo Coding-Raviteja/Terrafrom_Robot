87a0862d7f8f195982a8eb916478c23f248fb0bd
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-25 17:20:54 +0530

    Terrafrom_code

--- RoboTerraform/vars.tf
+++ RoboTerraform/vars.tf
@@ -19,5 +19,9 @@ variable "comp" {
             ami = "ami-08cd1a143ace74e19"
             instance_type = "t2.micro" 
         }
+        payment = {
+            ami = "ami-08cd1a143ace74e19"
+            #instance_type = "t2.micro" 
+        }
     } 
 }
