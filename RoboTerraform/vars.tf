23cd1b476e688c41e99556027f889343f5a346a9
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-25 17:56:37 +0530

    Terrafrom_code

--- RoboTerraform/vars.tf
+++ RoboTerraform/vars.tf
@@ -2,7 +2,12 @@ variable "comp" {
     default = {
         frontend = {
             ami = "ami-08cd1a143ace74e19"
-            instance_type = "t3.micro" 
+            instance_type = "t2.small" 
+        }
+
+        mysql = {
+            ami = "ami-08cd1a143ace74e19"
+            instance_type = "t2.small" 
         }
 
         cart = {
@@ -12,17 +17,36 @@ variable "comp" {
 
         user = {
             ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.micro" 
+            instance_type = "t2.small" 
         }
 
         payment = {
             ami = "ami-08cd1a143ace74e19"
-            #instance_type = "t2.micro" 
+            #instance_type = "t2.small" 
         }
 
         catalogue = {
             ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.micro" 
+            instance_type = "t2.small" 
+        }
+
+        redis = {
+            ami = "ami-08cd1a143ace74e19"
+            instance_type = "t2.small" 
+        }
+
+        mongodb = {
+            ami = "ami-08cd1a143ace74e19"
+            instance_type = "t2.small" 
+        }
+
+        shipping = {
+            ami = "ami-08cd1a143ace74e19"
+            instance_type = "t2.small" 
+        }
+        rabbitmq = {
+            ami = "ami-08cd1a143ace74e19"
+            instance_type = "t2.small" 
         }
     } 
 }
