fe344e1354acb6622213c9fe36d531fdb17a2700
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-25 17:16:06 +0530

    Terrafrom_code

--- RoboTerraform/ec2.tf
+++ RoboTerraform/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  count         = 10
-  ami           = "ami-08cd1a143ace74e19"
-  instance_type = "t3.micro"
+  count         = va
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
 
   tags = {
-    Name = "Test_Create-${count.index}"
+    Name = var.comp[each.key].name
   }
 }
