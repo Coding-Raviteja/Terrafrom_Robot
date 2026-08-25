a175ff10c288dc6da22d323155c6af99564fac84
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-25 17:51:34 +0530

    Terrafrom_code

--- Test/ec2.tf
+++ Test/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
   count         = 10
   ami           = "ami-08cd1a143ace74e19"
-  instance_type = "t3.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "Test_Create-${count.index}"
