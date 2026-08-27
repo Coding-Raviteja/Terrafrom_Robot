31413428763b1f569a594127ab7573e7b5ffce69
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-27 17:33:13 +0530

    Terrafrom_code

--- RoboTerraform_MultiEnv/main.tf
+++ RoboTerraform_MultiEnv/main.tf
@@ -2,6 +2,8 @@ module "ec2" {
     for_each = var.comp
 
     source =  "./modules"
+    name = each.key
+    comp = each.value
     ami_name = var.ami_name
     instance_type = var.instance_type
     sg_name       = var.sg_name
