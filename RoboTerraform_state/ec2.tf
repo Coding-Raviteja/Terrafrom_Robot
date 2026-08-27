8062c98e8f4c25c492536cba7aff9f1146b97210
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-27 19:59:41 +0530

    Terrafrom_code

--- RoboTerraform_state/ec2.tf
+++ RoboTerraform_state/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   for_each      = var.comp
-  ami           = each.value[data.aws_ami.latest.id]
+  ami           = each.value["data.aws_ami.latest.id"]
   instance_type = lookup(each.value, "instance_type", "t2.micro")
 
   tags = {
--- RoboTerraform_state/vars.tf
+++ RoboTerraform_state/vars.tf
@@ -50,3 +50,5 @@ variable "comp" {
         }
     } 
 }
+
+var
