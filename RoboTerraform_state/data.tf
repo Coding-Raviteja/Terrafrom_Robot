5bf1a0dc8cbd55336454edc9e233cb82bc25f545
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-27 20:13:25 +0530

    Terrafrom_code

--- RoboTerraform_state/data.tf
+++ RoboTerraform_state/data.tf
@@ -7,4 +7,5 @@ data "aws_ami" "latest" {
   }
 
   owners = ["355449129696"]
+  #owners = ["amazon"]
 }
--- RoboTerraform_state/vars.tf
+++ RoboTerraform_state/vars.tf
@@ -2,51 +2,51 @@ variable "comp" {
     default = {
         frontend = {
             ##ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         mysql = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         cart = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         user = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         payment = {
             #ami = "ami-08cd1a143ace74e19"
-            #instance_type = "t2.small" 
+            #instance_type = "t2.micro" 
         }
 
         catalogue = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         redis = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         mongodb = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
 
         shipping = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
         rabbitmq = {
             #ami = "ami-08cd1a143ace74e19"
-            instance_type = "t2.small" 
+            instance_type = "t2.micro" 
         }
     } 
 }
