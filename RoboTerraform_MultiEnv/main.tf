30dcb7a30143d4810a165630182d7f18d309e510
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-27 16:56:54 +0530

    Terrafrom_code

--- RoboTerraform_MultiEnv/main.tf
+++ RoboTerraform_MultiEnv/main.tf
@@ -2,7 +2,5 @@ module "ec2" {
   source =  "./modules"
   ami_name = var.ami_name
   instance_type = var.instance_type
-  env_name      = var.env_name
-  ami           = var.ami
   sg_name       = var.sg_name
 }
--- RoboTerraform_MultiEnv/modules/ec2.tf
+++ RoboTerraform_MultiEnv/modules/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "my_ec2" {
     ami           = data.aws_ami.my_ami.id
     instance_type = var.instance_type
+    vpc_security_group_ids = [data.aws_security_group.my_sg.id]
   tags = {
     Name = var.env_name
   }
