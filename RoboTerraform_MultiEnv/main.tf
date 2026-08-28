d780bc827acffa6f7510cacf8846cca91a3cbea3
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-28 22:37:24 +0530

    Terrafrom_code

--- RoboTerraform_MultiEnv/main.tf
+++ RoboTerraform_MultiEnv/main.tf
@@ -8,4 +8,6 @@ module "ec2" {
     instance_type = each.value["instance_type"]
     sg_name       = var.sg_name
     env_name      = var.env_name
+    domain_name   = var.domain_name
+    private_zone  = var.private_zone
 }
