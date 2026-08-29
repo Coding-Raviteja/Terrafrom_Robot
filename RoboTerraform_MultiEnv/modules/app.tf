fd904d32d881e44712d89eca24e5e116ccd23886
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-29 17:27:30 +0530

    Terrafrom_code

--- RoboTerraform_MultiEnv/modules/app.tf
+++ RoboTerraform_MultiEnv/modules/app.tf
@@ -1,11 +1,18 @@
 resource "null_resource" "main" {
-    depends_on = [aws_instance.main]
+    depends_on = [aws_instance.main, aws_route53_record.DNS]
 
     provisioner "remote-exec" {
+        connection {
+            type        = "ssh"
+            host        = aws_instance.main.private_ip
+            user        = "ec2-user"
+            password    = "DevOps321"  # Replace with your actual password or use a more secure method
+        }
         inline = [
-            "cd /tmp && echo Hello, World! && rm -rf /tmp/example.txt"
+            "pip3.11 install ansible",
+            "type ansible-playbook -v",
+            "ansible-pull -U https://github.com/Coding-Raviteja/Anisible_Robot.git 
         ]
-        command = "echo Hello, World! && rm -rf /tmp/example.txt"
     }
   
 }
