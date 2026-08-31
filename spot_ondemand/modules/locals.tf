c0f6cf0fcc47c5cc0ae06019a16df1efd419c594
DRT9999 <ravitejadevinidi@gmail.com>
2026-08-31 21:53:01 +0530

    Terrafrom_code

--- spot_ondemand/modules/locals.tf
+++ spot_ondemand/modules/locals.tf
@@ -1,3 +1,22 @@
+# locals {
+#   get_instance_ip = var.internal ? aws_instance.main.private_ip : aws_instance.main.public_ip
+# }
+
 locals {
-  get_instance_ip = var.internal ? aws_instance.main.private_ip : aws_instance.main.public_ip
+  on_demand_ips = [
+    for instance in aws_instance.main :
+    var.internal ? instance.private_ip : instance.public_ip
+  ]
+
+  spot_ips = [
+    for instance in aws_instance.spot :
+    var.internal ? instance.private_ip : instance.public_ip
+  ]
+
+  get_instance_ips = concat(
+    local.on_demand_ips,
+    local.spot_ips
+  )
+
+  get_instance_ip = length(local.get_instance_ips) > 0 ? local.get_instance_ips[0] : null
 }
